96bf03b59351c8183a0ad549232dc7b9465a43ec
butterdogg1467 <momink2600@gmail.com>
2024-07-08 18:35:14 -0400

    Made forecast visible on webpage and added styling

--- src/fetchforecast.js
+++ src/fetchforecast.js
@@ -5,9 +5,34 @@ export default function fetchForecast(){
     })
     .then(function(data){
         console.log(data)
+        date1.textContent = data.forecast.forecastday[0].date
+        avgtemp1.textContent = data.forecast.forecastday[0].day.avgtemp_c
+        forecasticon1.src = data.forecast.forecastday[0].day.condition.icon
+
+        
+        date2.textContent = data.forecast.forecastday[1].date
+        avgtemp2.textContent = data.forecast.forecastday[1].day.avgtemp_c
+        forecasticon2.src = data.forecast.forecastday[1].day.condition.icon
+
+        
+        date3.textContent = data.forecast.forecastday[2].date
+        avgtemp3.textContent = data.forecast.forecastday[2].day.avgtemp_c
+        forecasticon3.src = data.forecast.forecastday[2].day.condition.icon
     })
     .catch(function(err){
         console.log(err)
     })
 }
+
+const date1 = document.querySelector('#date1');
+const avgtemp1 = document.querySelector('#avgtemp1');
+const forecasticon1 = document.querySelector('#forecasticon1');
+
+const date2 = document.querySelector('#date2');
+const avgtemp2 = document.querySelector('#avgtemp2');
+const forecasticon2 = document.querySelector('#forecasticon2');
+
+const date3 = document.querySelector('#date3');
+const avgtemp3 = document.querySelector('#avgtemp3');
+const forecasticon3 = document.querySelector('#forecasticon3');
 
